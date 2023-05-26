060907dabf11d9846a26164c49c21badd1bbffd8
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-05-26 15:42:10 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: 'black'}}>Hello world</Text>
+    </View>
+  );
+}
+
+export default App;
